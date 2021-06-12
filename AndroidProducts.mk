fd027e0df445ff10fd792026f827b39c56826a66
rxuglr <rxuglr@mail.ru>
2021-06-12 18:25:56 +0300

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 RevengeOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/bootleg_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    bootleg_ginkgo-user \
+    bootleg_ginkgo-userdebug \
+    bootleg_ginkgo-eng
---
+++ bootleg_ginkgo.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 BootleggersROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from ginkgo device
+$(call inherit-product, device/xiaomi/ginkgo/device.mk)
+
+# Inherit some common BootleggersROM stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_PICK_BOOTANIMATION := "6"
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2340
+BOOTLEGGERS_BUILD_TYPE := Unshishufied
+$(call inherit-product, vendor/bootleggers/config/common.mk)
+
+
+# Device identifier
+PRODUCT_NAME := bootleg_ginkgo
+PRODUCT_DEVICE := ginkgo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 8
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
