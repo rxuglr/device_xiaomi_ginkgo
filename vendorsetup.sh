196f0195e3de4fb8dc791498c3b4482c959a5885
rxuglr <69907487+rxuglr@users.noreply.github.com>
2021-06-27 08:17:01 +0300

    Vendorsetup: Fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-cp hardware/qcom-caf/sm8150/Android.bp hardware/qcom-caf/sm8150_ginkgo
+cp hardware/qcom-caf/sm8150/Android.bp hardware/qcom-caf/sm8150_ginkgo/
 cp hardware/qcom-caf/sm8150/Android.mk hardware/qcom-caf/sm8150_ginkgo/
 
 
 # proton
-git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton --depth=1
+#git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton --depth=1
